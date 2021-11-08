03cc739dc0278c29424e23751f6761b2671658ff
goldenbull <goldenbull@gmail.com>
2021-11-08 16:00:05 +0800

    key status checking logic update

--- test0.py
+++ test0.py
@@ -1,21 +1,24 @@
 from microbit import *
-import time, music
+import time, music, radio
 
 
 class KeyStatus:
     """
-    记录各个按键的触发状态，get and clear模式
+    记录各个按键的触发状态，同步+轮询，get and clear模式
+    难点在于正确处理ab同时按下的事件
+    完整的行为应该通过状态机描述，但此处简单处理
     """
 
     def __init__(self):
-        self._prev_key_a_is_down = False
-        self._prev_key_b_is_down = False
+        self._prev_a_is_down = False
+        self._prev_b_is_down = False
+        self._prev_ab_is_down = False
         self._prev_logo_is_down = False
 
-        self._key_a_pressed = False
-        self._key_b_pressed = False
-        self._key_ab_pressed = False
-        self._logo_pressed = False
+        self._event_a = False
+        self._event_b = False
+        self._event_ab = False
+        self._event_logo = False
 
     def update(self):
         # 根据之前的状态和当前的状态，决定是否设置触发状态
@@ -23,45 +26,58 @@ class KeyStatus:
         cur_key_b_is_down = button_b.is_pressed()
         cur_logo_is_down = pin_logo.is_touched()
 
-        if self._prev_key_a_is_down and not cur_key_a_is_down:
-            self._key_a_pressed = True
-        if self._prev_key_b_is_down and not cur_key_b_is_down:
-            self._key_b_pressed = True
-        if (self._prev_key_a_is_down and self._prev_key_b_is_down) \
-                and not (cur_key_a_is_down and cur_key_b_is_down):
-            self._key_ab_pressed = True
-        if self._prev_logo_is_down and not cur_logo_is_down:
-            self._logo_pressed = True
+        # 首先特殊处理ab同时按下的情况
+        if self._prev_ab_is_down:
+            if not cur_key_a_is_down and not cur_key_b_is_down:
+                # 曾经同时按下，现在都抬起了
+                self._event_ab = True
+            else:
+                # 还有一个键没松开，等着松开
+                pass
+        else:
+            if cur_key_a_is_down and cur_key_b_is_down:
+                # 同时按下
+                self._prev_ab_is_down = True
+            else:
+                # 单独按键
+                if self._prev_a_is_down and not cur_key_a_is_down:
+                    self._event_a = True
+                if self._prev_b_is_down and not cur_key_b_is_down:
+                    self._event_b = True
 
-        self._prev_key_a_is_down = cur_key_a_is_down
-        self._prev_key_b_is_down = cur_key_b_is_down
+        # logo的逻辑相对简单
+        if self._prev_logo_is_down and not cur_logo_is_down:
+            self._event_logo = True
+
+        self._prev_a_is_down = cur_key_a_is_down
+        self._prev_b_is_down = cur_key_b_is_down
         self._prev_logo_is_down = cur_logo_is_down
 
     def is_key_a_pressed(self):
-        v = self._key_a_pressed
+        v = self._event_a
         if v:
-            self._key_a_pressed = False
+            self._event_a = False
         return v
 
     def is_key_b_pressed(self):
-        v = self._key_b_pressed
+        v = self._event_b
         if v:
-            self._key_b_pressed = False
+            self._event_b = False
         return v
 
     def is_key_ab_pressed(self):
-        # 同时clear a和b的状态
-        v = self._key_ab_pressed
+        # 同时clear单个键的状态
+        v = self._event_ab
         if v:
-            self._key_ab_pressed = False
-            self._key_a_pressed = False
-            self._key_b_pressed = False
+            self._event_ab = False
+            self._event_a = False
+            self._event_b = False
         return v
 
     def is_logo_pressed(self):
-        v = self._logo_pressed
+        v = self._event_logo
         if v:
-            self._logo_pressed = False
+            self._event_logo = False
         return v
 
 
@@ -214,9 +230,18 @@ class Clock:
                     display.set_pixel(7 - x, 4 - y, 0)
                 v = v // 2
 
+    def alarm(self, t0, t1):
+        h0, m0, s0 = t0
+        h1, m1, s1 = t1
+        ts0 = h0 * 3600 + m0 * 60 + s0
+        ts1 = h1 * 3600 + m1 * 60 + s1
+        if ts0 <= self.now() <= ts1:
+            music.play(["C6:1", "D6:1", "E6:1", ])
+
 
 # init_system
 pin_logo.set_touch_mode(pin_logo.CAPACITIVE)
+radio.off()  # to save energy
 
 # variables
 clock = Clock()
@@ -302,6 +327,9 @@ def main():
 
         # output
         clock.show()
+
+        # 闹钟
+        clock.alarm((7, 30, 0), (7, 30, 10))
 
         sleep(100)
 
